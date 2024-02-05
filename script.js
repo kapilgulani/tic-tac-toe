c5675114bd7ebfa0f90dea23cd567923d7119b91
kapilgulani <144957546+kapilgulani@users.noreply.github.com>
2024-02-05 11:56:09 -0800

    added files

---
+++ script.js
@@ -0,0 +1,57 @@
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+let currentPlayer = 'X';
+let gameOver = false;
+
+function makeMove(cell, row, col) {
+    if (board[row][col] || gameOver) {
+        return;
+    }
+    board[row][col] = currentPlayer;
+    cell.innerText = currentPlayer;
+    
+    if (checkWin()) {
+        document.getElementById('status').innerText = currentPlayer + " wins!";
+        gameOver = true;
+        return;
+    }
+    if (checkTie()) {
+        document.getElementById('status').innerText = "It's a tie!";
+        gameOver = true;
+        return;
+    }
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWin() {
+    // Check rows, columns, and diagonals
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+            return true;
+        }
+        if (board[0][i] && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+            return true;
+        }
+    }
+    if (board[0][0] && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+        return true;
+    }
+    if (board[0][2] && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+        return true;
+    }
+    return false;
+}
+
+function checkTie() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === '') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
